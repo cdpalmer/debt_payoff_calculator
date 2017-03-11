53ffafc02a9a00ad87d3f8c53a4d68932f37f081
Cody Palmer <cody.palmer08@gmail.com>
2017-03-11 16:10:27 -0600

    Including year estimate

--- payoff_estimation.rb
+++ payoff_estimation.rb
@@ -7,5 +7,5 @@ pmt = 5279
 puts "Paying $#{pmt} a month"
 calculator.calculate_payoff(pmt)
 
-puts; puts "Total months to total debt payoff: #{calculator.total_months}"
+puts; puts "Total months to total debt payoff: #{calculator.total_months} (#{(calculator.total_months/12.0).round(2)} years)"
 calculator.print_stats
